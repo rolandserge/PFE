55862655f470999cb0e0abc1e0a1422bbe1d0c76
sergeroland <sergeroland.komenan@gmail.com>
2023-06-17 18:53:08 +0000

    update code home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Home from './pages/Home';
+import Home from './pages/home';
 import Dashboad from './pages/dashbord';
 import FormationPage from './pages/formation';
 import Message from './pages/message'
